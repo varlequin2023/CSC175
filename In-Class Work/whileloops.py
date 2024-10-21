1fc58538783f48f6737d1770bc9e36c7655aa9d7
Vincent Arlequin <vincentarlequin@gmail.com>
2024-10-21 12:41:21 -0400

    Added 10-21-24 In Class Work

---
+++ In-Class Work/whileloops.py
@@ -0,0 +1,21 @@
+# While Loops - FA/24 CSC-175-02 - 10/21/24
+
+print("Example 1") # Example 1
+x = 1
+while x < 5:
+    x = x + 1
+print(x)
+# 5 should print
+
+print("Example 2") # Example 2
+x = 1
+while x < 5:
+    print(x)
+# 1 will repeatedly print forever
+
+print("Example 3") # Example 3
+x = 1
+while x < 5:
+    x = x - 1
+print(x)
+# Nothing will print - infinite loop
